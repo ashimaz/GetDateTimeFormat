3d8373c0f0ac8d118ce545524b58f7436b8d9312
ashimaz <ashimaz@hotmail.com>
2018-10-27 12:21:34 +0500

    Add project files.

---
+++ GetDateTimeFormat/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GetDateTimeFormat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var d1 = DateTime.Now;
+            var str = $"_{d1.Year}{d1.Month}{d1.Month}.{d1.Hour}{d1.Minute}";
+
+            TextBox1.Text = str;
+            
+            Clipboard.SetText(str);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var d1 = DateTime.Now;
+            var str = $"_{d1.Year}{d1.Month}{d1.Month}.{d1.Hour}{d1.Minute}";
+
+            TextBox1.Text = str;
+            
+            Clipboard.SetText(str);
+        }
+    }
+}
